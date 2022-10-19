0cf3340c8900bf766eb0fa26002d73dae3a332ef
Amarildo Antonio Zolet Junior <amarildozj@gmail.com>
2022-10-19 12:39:26 -0300

    Implementação de autenticação do usuario

--- Utilizando MySQL/conexao/conexao.js
+++ Utilizando MySQL/conexao/conexao.js
@@ -1,6 +1,6 @@
 const sequelize = require("sequelize");
 
-const conexao = new sequelize("testeapi","root","Junior123",{
+const conexao = new sequelize("testeapi","root","junior123",{
    host: "localhost",
    dialect: "mysql",
 });
--- Utilizando MySQL/start.js
+++ Utilizando MySQL/start.js
@@ -7,7 +7,7 @@ const jwtSecret = "testeparaapi"
 
 const app = express();
 
-const ip = "192.168.1.6";
+const ip = "192.168.0.100";
 const PORT = process.env.PORT || 3000;
 
 
@@ -20,38 +20,51 @@ app.use(cors())
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-
+//middleware de validação
 function auth(req,res,next){
     const authToken = req.headers['authorization'];
-    
+    if(authToken != undefined){
+        let arrayToken = authToken.split(" ");
+        let token = arrayToken[1];
+        jwt.verify(token,jwtSecret,(erro,data)=>{
+            if(erro){
+                res.statusCode = 401;
+        res.sendStatus(401);
+            }else{
+                res.statusCode = 200;
+                req.usuarioLogado = {id: data.id,email: data.email}
+                next();
+            }
+        })
+    }else{
+        res.statusCode = 401;
+        res.sendStatus(401);
+    }
 }
 
 
 //Listar todos
-app.get("/games", (req, res) => {
+app.get("/games",auth,(req, res) => {
     tabela.findAll().then((resposta) => {
-        res.json(resposta);
+        res.json({games:resposta,usuario:req.usuarioLogado});
     })
 })
 
 //Listar específico
-app.get("/game/:id", (req, res) => {
+app.get("/game/:id",auth, (req, res) => {
     let id = req.params.id;
     let convertido = parseInt(id);
     if (!isNaN(convertido)) {
         tabela.findOne({ where: { id: convertido } }).then((resposta) => {
             if (resposta != undefined) {
                 res.statusCode = 200;
-                console.log("Encontrei");
                 res.json(resposta)
             } else {
-                console.log("Erro");
                 res.statusCode = 400;
                 res.sendStatus(400);
             }
         })
     } else {
-        console.log("Erro");
         res.statusCode = 400;
         res.sendStatus(400)
     }
@@ -59,7 +72,7 @@ app.get("/game/:id", (req, res) => {
 
 
 //Criar um game
-app.post("/game", (req, res) => {
+app.post("/game",auth, (req, res) => {
     let { nome, ano } = req.body;
     if (nome !== undefined && nome !== null && nome.length > 1) {
         if (ano !== undefined && ano !== null && ano.length > 1) {
@@ -67,30 +80,26 @@ app.post("/game", (req, res) => {
                 nome: nome,
                 ano: ano,
             });
-            console.log("Ok");
             res.statusCode = 200;
             res.sendStatus(200);
         } else {
-            console.log("Erro");
             res.statusCode = 400;
             res.sendStatus(400);
         }
     } else {
-        console.log("Erro");
         res.statusCode = 400;
         res.sendStatus(400);
     }
 })
 
 //Deletar um game
-app.delete("/game/:id", (req, res) => {
+app.delete("/game/:id",auth, (req, res) => {
     let id = req.params.id;
     let convertido = parseInt(id);
     if (!isNaN(convertido)) {
         tabela.destroy({ where: { id: convertido } }).then((resposta) => {
             if (resposta != 0) {
                 res.statusCode = 200;
-                console.log("Deletado");
                 res.json("Deletei este cara aqui\n" + resposta);
             } else {
                 console.log("Erro");
@@ -99,14 +108,13 @@ app.delete("/game/:id", (req, res) => {
             }
         });
     } else {
-        console.log("Erro");
         res.statusCode = 400;
         res.sendStatus(400)
     }
 })
 
 //Editar um game
-app.put("/game/:id", (req, res) => {
+app.put("/game/:id",auth, (req, res) => {
     let id = req.params.id;
     let convertido = parseInt(id);
     let { nome, ano } = req.body
@@ -123,23 +131,19 @@ app.put("/game/:id", (req, res) => {
                             res.sendStatus(200);
                         })
                     } else {
-                        console.log("Erro");
                         res.statusCode = 404;
                         res.sendStatus(404)
                     };
                 });
             } else {
-                console.log("Erroa");
                 res.statusCode = 400;
                 res.sendStatus(400)
             };
         } else {
-            console.log("Erro");
             res.statusCode = 400;
             res.sendStatus(400)
         };
     } else {
-        console.log("Erro");
         res.statusCode = 400;
         res.sendStatus(400)
     };
@@ -159,7 +163,7 @@ app.post("/auth",(req,res)=>{
                         res.sendStatus(401);
                     }else{
                         res.statusCode = 200;
-                        return res.json({token:token});
+                        res.json({token:token});
                     }
                 });
             }else{
@@ -172,6 +176,27 @@ app.post("/auth",(req,res)=>{
         }else{
             res.statusCode = 404;
             res.sendStatus(404);
+        }
+    }else{
+        res.statusCode = 404;
+        res.sendStatus(404);
+    }
+})
+
+app.post("/create",(req,res)=>{
+    let {email,senha} = req.body;
+    let regexEmail = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/gi;
+    if(email !== undefined && email !== null && regexEmail.test(email) === true){
+        if(senha !== undefined && senha !== null && senha.length > 3){
+            res.statusCode = 201;
+            res.sendStatus(201);
+            usuarios.create({
+                email: req.body.email,
+                senha: req.body.senha
+            })
+        }else{
+            res.statusCode = 404;
+        res.sendStatus(404);
         }
     }else{
         res.statusCode = 404;
